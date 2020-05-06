a5b36230fa2d924a1d1c7b08c295ad078f890432
zgtcktom <ckt.tomchung@gmail.com>
2020-05-06 17:33:16 +0800

    Add files via upload

---
+++ src/LSRCompute.java
@@ -0,0 +1,253 @@
+import javafx.util.Pair;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.*;
+
+public class LSRCompute {
+    public static class Network {
+        public static class Node {
+            private final String name;
+            private final List<Pair<Node, Integer>> links;
+
+            // check if a node is in the links
+            public boolean has(Node node) {
+                for (Pair<Node, Integer> link : links)
+                    if (link.getKey() == node) return true;
+                return false;
+            }
+
+            // add a node to the links
+            public void connect(Node node, Integer cost) {
+                if (!has(node)) {
+                    links.add(new Pair<>(node, cost));
+                    links.sort(Comparator.comparing(a -> a.getKey().getName()));
+                }
+            }
+
+            // remove a node from the links
+            public void disconnect(Node node) {
+                links.removeIf(link -> link.getKey() == node);
+            }
+
+            // constructor
+            public Node(String name) {
+                this.name = name;
+                this.links = new ArrayList<>();
+            }
+
+            public String getName() {
+                return name;
+            }
+
+            public List<Pair<Node, Integer>> getLinks() {
+                return links;
+            }
+
+            @Override
+            public String toString() {
+                List<String> list = new ArrayList<>();
+                for (Pair<Node, Integer> link : links) {
+                    list.add(link.getKey().getName() + ':' + link.getValue());
+                }
+                return name + ": " + String.join(" ", list);
+            }
+        }
+
+        private List<Node> nodes;
+
+        // break link between 2 nodes (bidirectional)
+        public void breakLink(String a, String b) {
+            if (has(a) && has(b)) {
+                get(a).disconnect(get(b));
+                get(b).disconnect(get(a));
+            }
+        }
+
+        // remove the node and break link all the links (bidirectional)
+        public void remove(String name) {
+            if (has(name)) {
+                for (Node node : nodes) {
+                    node.disconnect(get(name));
+                }
+            }
+            nodes.removeIf(node -> node.getName().equals(name));
+        }
+
+        // check if a node is in the network
+        public boolean has(String name) {
+            for (Node node : nodes)
+                if (node.getName().equals(name)) return true;
+            return false;
+        }
+
+        // get node by name
+        public Node get(String name) {
+            for (Node node : nodes)
+                if (node.getName().equals(name)) return node;
+            return null;
+        }
+
+        // get node by index
+        public Node get(Integer i) {
+            return nodes.get(i);
+        }
+
+        // get all names
+        public List<String> getNames() {
+            List<String> names = new ArrayList<>();
+            for (Node node : nodes) {
+                names.add(node.getName());
+            }
+            return names;
+        }
+
+        // add node by name (or return the existing node) and return the new node
+        public Node add(String name) {
+            if (has(name)) {
+                return get(name);
+            }
+            Node node = new Node(name);
+            nodes.add(node);
+            nodes.sort(Comparator.comparing(Node::getName));
+            return node;
+        }
+
+        public Integer indexOf(Node node) {
+            return nodes.indexOf(node);
+        }
+
+        public Network() {
+            init();
+        }
+
+        @Override
+        public String toString() {
+            List<String> list = new ArrayList<>();
+            for (Node node : nodes) {
+                list.add(node.toString());
+            }
+            return String.join("\n", list);
+        }
+
+        // load from single line "H: F:9 E:2"
+        public void load(String data) {
+            String[] links = data.split(" ");
+            Node node = add(links[0].substring(0, links[0].length() - 1));
+            for (int i = 1; i < links.length; i++) {
+                String[] params = links[i].split(":");
+                Node neighbor = add(params[0]);
+                Integer cost = Integer.parseInt(params[1]);
+                node.connect(neighbor, cost);
+                neighbor.connect(node, cost);
+            }
+        }
+
+        public void init() {
+            nodes = new ArrayList<>();
+        }
+    }
+
+    public Network network;
+
+    // load from file
+    public void load(File file) throws FileNotFoundException {
+        Scanner scanner = new Scanner(file);
+        while (scanner.hasNext()) {
+            String line = scanner.nextLine();
+            network.load(line);
+        }
+    }
+
+    boolean useGUI = false;
+
+    public void dijkstra(String src, boolean waitKey) {
+        List<String> names = network.getNames();
+
+        int n = names.indexOf(src);
+        int N = names.size();
+
+        int[] dist = new int[N];
+        boolean[] visited = new boolean[N];
+        List<String>[] path = new ArrayList[N];
+
+        for (int i = 0; i < N; i++) {
+            dist[i] = Integer.MAX_VALUE;
+            visited[i] = false;
+        }
+
+        dist[n] = 0;
+        path[n] = new ArrayList<>();
+        path[n].add(src);
+
+        if (waitKey) log("Steps");
+
+        for (int i = 0; i < N; i++) {
+            int minDist = Integer.MAX_VALUE, minInd = -1;
+
+            for (int j = 0; j < N; j++) {
+                if (!visited[j] && dist[j] < minDist) {
+                    minDist = dist[j];
+                    minInd = j;
+                }
+            }
+
+            visited[minInd] = true;
+
+            for (Pair<Network.Node, Integer> pair : network.get(minInd).getLinks()) {
+                Network.Node node = pair.getKey();
+                Integer cost = pair.getValue();
+
+                int k = network.indexOf(node);
+                if (dist[minInd] + cost < dist[k]) {
+                    String name = pair.getKey().getName();
+                    dist[k] = dist[minInd] + cost;
+
+                    path[k] = new ArrayList<>();
+                    path[k].addAll(path[minInd]);
+                    path[k].add(name);
+
+                    if (waitKey) {
+                        log("Found " + name + ": Path: " + String.join(">", path[k]) + " Cost: " + dist[k], "");
+                        if (useGUI) {
+                            log("");
+                        } else {
+                            log(" [press any key to continue]", "");
+                            new Scanner(System.in).nextLine();
+                        }
+                    }
+                }
+            }
+        }
+
+        if (waitKey) log("\nSummary table");
+        log("Source " + src);
+        for (int i = 0; i < N; i++) {
+            if (i == n) continue;
+            log(names.get(i) + ": Path: " + String.join(">", path[i]) + " Cost: " + dist[i]);
+        }
+    }
+
+    public LSRCompute() {
+        network = new Network();
+    }
+
+    public void log(String str) {
+        log(str, "\n");
+    }
+
+    public void log(String str, String end) {
+        System.out.print(str + end);
+    }
+
+    public static void main(String[] args) throws FileNotFoundException {
+        if (args.length < 2) {
+            throw new IllegalArgumentException("The format should be: java LSRCompute [filepath] [source_node] [SS|CA]");
+        }
+
+        LSRCompute lsr = new LSRCompute();
+
+        lsr.load(new File(args[0]));
+        lsr.dijkstra(args[1], (args.length > 2) && (args[2].equals("SS")));
+    }
+}
---
+++ src/Main.java
@@ -0,0 +1,167 @@
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.ChoiceBox;
+import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
+import javafx.stage.FileChooser;
+import javafx.stage.Stage;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.PrintWriter;
+import java.util.List;
+
+public class Main extends Application {
+    LSRCompute lsr;
+
+    ChoiceBox<String> boxSrc;
+    Button btnLoad, btnSave, btnSingle, btnAll, btnNew, btnRemove, btnBreak;
+    TextField textNew, textRemove, textBreak;
+    TextArea txtContent, txtStatus;
+
+    private void reset() {
+        txtStatus.clear();
+        txtStatus.setScrollTop(txtStatus.getScrollTop());
+    }
+
+    private void ready(Scene scene) {
+        boxSrc = (ChoiceBox<String>) scene.lookup("#box_src");
+        btnLoad = (Button) scene.lookup("#btn_load");
+        btnSave = (Button) scene.lookup("#btn_save");
+        btnSingle = (Button) scene.lookup("#btn_single");
+        btnAll = (Button) scene.lookup("#btn_all");
+        btnNew = (Button) scene.lookup("#btn_new");
+        btnRemove = (Button) scene.lookup("#btn_remove");
+        btnBreak = (Button) scene.lookup("#btn_break");
+        textNew = (TextField) scene.lookup("#text_new");
+        textRemove = (TextField) scene.lookup("#text_remove");
+        textBreak = (TextField) scene.lookup("#text_break");
+        txtContent = (TextArea) scene.lookup("#txt_content");
+        txtStatus = (TextArea) scene.lookup("#txt_status");
+    }
+
+    private void update() {
+        txtContent.setText(lsr.network.toString());
+        String value = boxSrc.getValue();
+        List<String> names = lsr.network.getNames();
+        boxSrc.getItems().clear();
+        names.forEach(name -> boxSrc.getItems().add(name));
+
+        if (boxSrc.getItems().isEmpty()) boxSrc.setValue(null);
+        else if (value != null && names.contains(value)) boxSrc.setValue(value);
+        else boxSrc.setValue(names.get(0));
+    }
+
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
+        Scene scene = new Scene(root, 700, 400);
+        primaryStage.setTitle("LSRCompute");
+        primaryStage.setScene(scene);
+        primaryStage.show();
+        ready(scene);
+
+        lsr = new LSRCompute() {
+            @Override
+            public void log(String str, String end) {
+                txtStatus.appendText(str + end);
+            }
+        };
+        lsr.useGUI = true;
+
+        FileChooser fileChooser = new FileChooser();
+        fileChooser.setInitialDirectory(new File(System.getProperty("user.home")));
+
+        btnLoad.setOnAction(e -> {
+            File file = fileChooser.showOpenDialog(primaryStage);
+            if (file != null) {
+                fileChooser.setInitialDirectory(file.getParentFile());
+                try {
+                    lsr.network.init();
+                    lsr.load(file);
+                    update();
+                } catch (FileNotFoundException fileNotFoundException) {
+                    fileNotFoundException.printStackTrace();
+                }
+            }
+        });
+
+        btnSave.setOnAction(e -> {
+            File file = fileChooser.showSaveDialog(primaryStage);
+            if (file != null) {
+                fileChooser.setInitialDirectory(file.getParentFile());
+                try {
+                    PrintWriter writer = new PrintWriter(file);
+                    writer.print(lsr.network);
+                    writer.close();
+                } catch (FileNotFoundException fileNotFoundException) {
+                    fileNotFoundException.printStackTrace();
+                }
+            }
+        });
+
+        btnSingle.setOnAction(e -> {
+            reset();
+            lsr.dijkstra(boxSrc.getValue(), true);
+        });
+
+        btnAll.setOnAction(e -> {
+            reset();
+            lsr.dijkstra(boxSrc.getValue(), false);
+        });
+
+        btnNew.setOnAction(e -> {
+            reset();
+            String input = textNew.getText();
+            if (input != null && input.length() > 0) {
+                lsr.network.load(input);
+                lsr.log("Added node " + input);
+                update();
+            } else {
+                lsr.log("Empty field");
+            }
+            // H: F:9 E:2
+        });
+
+        btnRemove.setOnAction(e -> {
+            reset();
+            String input = textRemove.getText();
+            if (input != null && input.length() > 0) {
+                if (lsr.network.has(input)) {
+                    lsr.network.remove(input);
+                    lsr.log("Removed node " + input);
+                } else {
+                    lsr.log("Node " + input + " does not exist");
+                }
+                update();
+            } else {
+                lsr.log("Empty field");
+            }
+            // D
+        });
+
+        btnBreak.setOnAction(e -> {
+            reset();
+            String input = textBreak.getText();
+            if (input != null && input.length() > 0) {
+                String[] names = input.split(">");
+                for (int i = 0; i < names.length - 1; i++) {
+                    lsr.network.breakLink(names[i], names[i + 1]);
+                    lsr.log("Removed link " + names[i] + ">" + names[i + 1]);
+                }
+                update();
+            } else {
+                lsr.log("Empty field");
+            }
+            // C>D
+        });
+    }
+
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
